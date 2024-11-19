1005dfd91ebc9e9d54f825f153d4401387d15722
Delphox <smugleaf@live.com>
2024-11-19 11:20:32 -0300

    bruh

--- Aliucord/src/main/java/com/aliucord/coreplugins/rn/Patches.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/rn/Patches.kt
@@ -205,6 +205,7 @@ fun patchStickers() {
     }
     Patcher.addPatch(Sticker::class.java.getDeclaredMethod("a"), hook)
     Patcher.addPatch(StickerPartial::class.java.getDeclaredMethod("a"), hook)
+        val formatType = Sticker::class.java.getDeclaredField("formatType").apply { isAccessible = true }
     Patcher.addPatch(Sticker::class.java.getDeclaredMethod("b"), PreHook {
         if (formatType[it.thisObject] == StickerFormatType.UNKNOWN) it.result = ".gif"
     })
